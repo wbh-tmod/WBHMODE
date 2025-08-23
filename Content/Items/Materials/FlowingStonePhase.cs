5bb8b983cef1065d1b091d223963499dbe0c7763
wbh-tmod <wbh123456kevin@qq.com>
2025-08-23 21:07:42 +0800

    添加流动石相FlowingStonePhase

---
+++ Content/Items/Materials/FlowingStonePhase.cs
@@ -0,0 +1,47 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using WBHMODE.Content.Tiles;
+
+namespace WBHMODE.Content.Items.Materials
+{
+    public class FlowingStonePhase : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 69; // Hellstone
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 9999;
+            Item.value = Item.sellPrice(silver: 1); // 售价
+            Item.rare = ItemRarityID.Blue;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.useTurn = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.autoReuse = true;
+            Item.consumable = true;
+        }
+        public override void AddRecipes()
+        {
+#if DEBUG
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 1); // 合成配方
+            recipe.AddTile(TileID.WorkBenches); // 合成台
+            recipe.ReplaceResult(ModContent.ItemType<FlowingStonePhase>(), 100);
+            recipe.Register();
+#endif
+        }
+    }
+}
