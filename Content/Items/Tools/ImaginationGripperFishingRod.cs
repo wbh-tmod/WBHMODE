03cac4ab1fc631fbbb7fcc68b8b501ea05200215
wbh-tmod <wbh123456kevin@qq.com>
2025-08-24 20:52:07 +0800

    添加臆想抓手ImaginationGripperFishingRod

---
+++ Content/Items/Tools/ImaginationGripperFishingRod.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using WBHMODE.Content.Items.Materials;
+using WBHMODE.Content.Projectiles;
+
+namespace WBHMODE.Content.Items.Tools
+{
+    public class ImaginationGripperFishingRod : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.CanFishInLava[Item.type] = true; // Allows the pole to fish in lava
+        }
+        public override void SetDefaults()
+        {
+            Item.CloneDefaults(ItemID.WoodFishingPole);
+
+            Item.value = Item.sellPrice(gold: 2, silver: 80); // 售价
+            Item.rare = ItemRarityID.Blue;
+            Item.fishingPole = 24; // Sets the poles fishing power
+            Item.shootSpeed = 14f; // Sets the speed in which the bobbers are launched. Wooden Fishing Pole is 9f and Golden Fishing Rod is 17f.
+            Item.shoot = ModContent.ProjectileType<Projectiles.ImaginationGripperBobber>(); // The bobber projectile. Note that this will be overridden by Fishing Bobber accessories if present, so don't assume the bobber spawned is the specified projectile. https://terraria.wiki.gg/wiki/Fishing_Bobbers
+        }
+        public override void HoldItem(Player player)
+        {
+            player.accFishingLine = true;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            //int bobberAmount = Main.rand.Next(3, 6); // 3 to 5 bobbers
+            int bobberAmount = 1;
+            float spreadAmount = 75f; // how much the different bobbers are spread out.
+            Vector2 bobberSpeed = velocity + new Vector2(Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f, Main.rand.NextFloat(-spreadAmount, spreadAmount) * 0.05f);
+            // Generate new bobbers
+            Projectile.NewProjectile(source, position, bobberSpeed, type, 0, 0f, player.whoAmI);
+            return false;
+        }
+        public override void ModifyFishingLine(Projectile bobber, ref Vector2 lineOriginOffset, ref Color lineColor)
+        {
+            // Change these two values in order to change the origin of where the line is being drawn.
+            // This will make it draw 43 pixels right and 30 pixels up from the player's center, while they are looking right and in normal gravity.
+            lineOriginOffset = new Vector2(43, -30);
+
+            // Sets the fishing line's color. Note that this will be overridden by the colored string accessories.
+            if (bobber.ModProjectile is ImaginationGripperBobber imaginationGripperBobber)
+            {
+                // ExampleBobber has custom code to decide on a line color.
+                lineColor = imaginationGripperBobber.FishingLineColor;
+            }
+            else
+            {
+                // If the bobber isn't ExampleBobber, a Fishing Bobber accessory is in effect and we use DiscoColor instead.
+                lineColor = Main.DiscoColor;
+            }
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<LiquidGoldBar>(), 8)
+                .AddTile(TileID.Anvils)
+                .Register();
+#if DEBUG
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<LiquidGoldBar>(), 8)
+                .Register();
+#endif
+        }
+    }
+}
---
+++ Content/Projectiles/ImaginationGripperBobber.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBHMODE.Content.Projectiles
+{
+    public class ImaginationGripperBobber : ModProjectile
+    {
+   //     public static readonly Color[] PossibleLineColors = [
+   //         new Color(255, 215, 0), // A gold color
+			//new Color(0, 191, 255) // A blue color
+   //     ];
+        //private int fishingLineColorIndex;
+
+        //public Color FishingLineColor => PossibleLineColors[fishingLineColorIndex];
+        public Color FishingLineColor => Color.Gray;
+
+        public override void SetDefaults()
+        {
+            // These are copied through the CloneDefaults method
+            // Projectile.width = 14;
+            // Projectile.height = 14;
+            // Projectile.aiStyle = 61;
+            // Projectile.bobber = true;
+            // Projectile.penetrate = -1;
+            // Projectile.netImportant = true;
+            Projectile.CloneDefaults(ProjectileID.BobberWooden);
+
+            DrawOriginOffsetY = -8; // Adjusts the draw position
+        }
+        //public override void OnSpawn(IEntitySource source)
+        //{
+        //    // Decide color of the pole by getting the index of a random entry from the PossibleLineColors array.
+        //    fishingLineColorIndex = (byte)Main.rand.Next(PossibleLineColors.Length);
+        //}
+        public override void AI()
+        {
+            // Always ensure that graphics-related code doesn't run on dedicated servers via this check.
+            if (!Main.dedServ)
+            {
+                // Create some light based on the color of the line.
+                Lighting.AddLight(Projectile.Center, FishingLineColor.ToVector3());
+            }
+        }
+        //public override void SendExtraAI(BinaryWriter writer)
+        //{
+        //    writer.Write((byte)fishingLineColorIndex);
+        //}
+
+        //public override void ReceiveExtraAI(BinaryReader reader)
+        //{
+        //    fishingLineColorIndex = reader.ReadByte();
+        //}
+    }
+}
