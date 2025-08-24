8eab78991c07e2eda535c2301e600874db853078
wbh-tmod <wbh123456kevin@qq.com>
2025-08-24 12:08:33 +0800

    添加删除镐EliminationPickaxe

---
+++ Content/Items/Tools/EliminationPickaxe.cs
@@ -0,0 +1,55 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using WBHMODE.Content.Items.Materials;
+
+namespace WBHMODE.Content.Items.Tools
+{
+    public class EliminationPickaxe : ModItem
+    {
+        public override void SetDefaults() {
+            Item.damage = 15;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 40;
+            Item.height = 40;
+            Item.useTime = 24;
+            Item.useAnimation = 13;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4;
+            Item.value = Item.buyPrice(silver: 36); // Buy this item for one gold - change gold to any coin and change the value to any number <= 100
+            Item.rare = ItemRarityID.Blue;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+
+            Item.pick = 75; // How strong the pickaxe is, see https://terraria.wiki.gg/wiki/Pickaxe_power for a list of common values
+            Item.attackSpeedOnlyAffectsWeaponAnimation = true; // Melee speed affects how fast the tool swings for damage purposes, but not how fast it can dig
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox) {
+            if (Main.rand.NextBool(10)) {
+                Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, DustID.Ash);
+            }
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<LiquidGoldBar>(), 12)
+                .AddIngredient(ModContent.ItemType<FlowingStonePhase>(), 6)
+                .AddTile(TileID.Anvils)
+                .Register();
+#if DEBUG
+            CreateRecipe()
+                .AddIngredient(ModContent.ItemType<LiquidGoldBar>(), 12)
+                .AddIngredient(ModContent.ItemType<FlowingStonePhase>(), 6)
+                .Register();
+#endif
+        }
+    }
+}
