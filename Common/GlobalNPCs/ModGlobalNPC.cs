69528abd8b366c256623f483cbd2a0b26e981380
wbh-tmod <wbh123456kevin@qq.com>
2025-09-07 12:25:59 +0800

    添加半衰HalfDeclineDebuff 该Debuff可能存在数个bug

--- Common/GlobalNPCs/ModGlobalNPC.cs
+++ Common/GlobalNPCs/ModGlobalNPC.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿#define DEBUG
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,20 +15,26 @@ namespace WBHMODE.Common.GlobalNPCs
     {
         public override bool InstancePerEntity => true;
         public bool acidEtchingDebuff;
+        public bool halfDeclineDebuff;
+        public int halfDeclineFlag;
+        public int lifeMax2; // original lifeMax
         public override void ResetEffects(NPC npc) {
             acidEtchingDebuff = false;
+            halfDeclineDebuff = false;
+            halfDeclineFlag = 0;
+            lifeMax2 = npc.lifeMax;
         }
         public override void UpdateLifeRegen(NPC npc, ref int damage)
         {
             if (acidEtchingDebuff)
             {
-                //if (npc.lifeRegen > 0)
-                //{
-                //    npc.lifeRegen = 0;
-                //}
                 float v = (float)Math.Sqrt(npc.velocity.X * npc.velocity.X + npc.velocity.Y * npc.velocity.Y);
-                npc.lifeRegen = -(int)v;
+                npc.lifeRegen -= (int)v;
             }
+        }
+        public override void OnHitPlayer(NPC npc, Player target, Player.HurtInfo hurtInfo)
+        {
+            npc.ai[0] = 1f;
         }
     }
 }
---
+++ Content/Buffs/HalfDeclineDebuff.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using WBHMODE.Common.GlobalNPCs;
+
+namespace WBHMODE.Content.Buffs
+{
+    public class HalfDeclineDebuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = false;
+            BuffID.Sets.GrantImmunityWith[Type].Add(ModContent.BuffType<HalfDeclineDebuff>());
+        }
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            //ModGlobalNPC modGlobalNPC = npc.GetGlobalNPC<ModGlobalNPC>();
+            npc.GetGlobalNPC<ModGlobalNPC>().halfDeclineDebuff = true;
+            //ref float AI_State = ref npc.ai[0];
+            //ref float AI_Timer = ref npc.ai[1];
+            npc.ai[1]++;
+            if (npc.ai[1] == 60f) {
+                float percent = npc.life / (npc.lifeMax * 1.0f);
+                if (npc.lifeMax * 2 > npc.GetGlobalNPC<ModGlobalNPC>().lifeMax2)
+                {
+                    npc.lifeMax -= Math.Max(npc.GetGlobalNPC<ModGlobalNPC>().lifeMax2 / 100, 1);
+                    npc.life = (int)(percent * npc.lifeMax);
+                    npc.ai[1] = 0;
+                } else {
+                    npc.ai[1] = 60;
+                }
+            }
+            //if (npc.GetGlobalNPC<ModGlobalNPC>().halfDeclineFlag != 0) {
+            //    npc.ai[0] = 1f;
+            //}
+            if (npc.ai[0] == 1f) {
+                float percent = npc.life / (npc.lifeMax * 1.0f);
+                npc.lifeMax = npc.GetGlobalNPC<ModGlobalNPC>().lifeMax2;
+                npc.life = (int)(percent * npc.lifeMax);
+                //npc.life = npc.lifeMax;
+                npc.ai[1] = 0;
+                npc.ai[0] = 0;
+            }
+        }
+    }
+}
