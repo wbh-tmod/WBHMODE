a4b717c86b669c938ba0f651f205ee6161342afd
wbh-tmod <wbh123456kevin@qq.com>
2025-10-10 20:43:14 +0800

    添加硬化流动沙块HardenedFlowingSandBlock 添加沙漠火把对应的合成配方

---
+++ Content/Items/Placeable/HardenedFlowingSandBlock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBHMODE.Content.Items.Placeable
+{
+    public class HardenedFlowingSandBlock : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+        }
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.HardenedFlowingSand>());
+            Item.width = 12;
+            Item.height = 12;
+            Item.ammo = AmmoID.Sand;
+        }
+        public override void AddRecipes()
+        {
+#if DEBUG
+            CreateRecipe(100).
+                AddIngredient(ItemID.DirtBlock, 1).
+                AddTile(TileID.WorkBenches).
+                Register();
+#endif
+            // 硬化流动沙块-硬化沙块
+            CreateRecipe().
+                AddIngredient(ModContent.ItemType<HardenedFlowingSandBlock>(), 1).
+                AddTile(TileID.ChlorophyteExtractinator).
+                Register().
+                ReplaceResult(ItemID.HardenedSand);
+            // 硬化流动沙块-沙漠火把
+            CreateRecipe(3).
+                AddIngredient(ModContent.ItemType<HardenedFlowingSandBlock>(), 1).
+                AddIngredient(ItemID.Torch, 3).
+                Register().
+                ReplaceResult(ItemID.DesertTorch);
+        }
+    }
+}
---
+++ Content/Tiles/HardenedFlowingSand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using WBHMODE.Content.Projectiles;
+
+namespace WBHMODE.Content.Tiles
+{
+    public class HardenedFlowingSand : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileBrick[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            // Sand specific properties
+            Main.tileSand[Type] = true;
+            TileID.Sets.Conversion.Sand[Type] = true; // Allows Clentaminator solutions to convert this tile to their respective Sand tiles.
+            TileID.Sets.ForAdvancedCollision.ForSandshark[Type] = true; // Allows Sandshark enemies to "swim" in this sand.
+            TileID.Sets.CanBeDugByShovel[Type] = true;
+            TileID.Sets.Suffocate[Type] = true;
+            
+            TileID.Sets.CanBeClearedDuringOreRunner[Type] = true;
+            TileID.Sets.GeneralPlacementTiles[Type] = false;
+            TileID.Sets.ChecksForMerge[Type] = true;
+
+            MineResist = 0.5f; // Sand tile typically require half as many hits to mine.
+            DustType = DustID.Stone;
+            AddMapEntry(new Color(150, 150, 150));
+        }
+        public override bool HasWalkDust()
+        {
+            return Main.rand.NextBool(3);
+        }
+        public override void WalkDust(ref int dustType, ref bool makeDust, ref Color color)
+        {
+            dustType = DustID.Sand;
+        }
+    }
+}
