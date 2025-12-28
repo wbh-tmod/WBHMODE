c426c8ceeb8cb2bd804193c72caa8550da488690
lxklp <andrew20040528@163.com>
2025-12-28 20:02:02 +0800

    添加了虚粉和共享思维。虚粉的投射物和物块转化以及共享思维的boss召唤暂时没做。

---
+++ Content/Items/Materials/SharedThoughts.cs
@@ -0,0 +1,68 @@
+﻿//#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WBHMODE.Content.Tiles;
+
+namespace WBHMODE.Content.Items.Materials
+{
+    public class SharedThoughts: ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 3;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 69; // Hellstone
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 9999;
+            Item.value = Item.buyPrice(copper: 0); // 售价
+            Item.rare = ItemRarityID.Blue;
+            Item.useTurn = true;
+            Item.autoReuse = false;
+            Item.consumable = true;
+            Item.useAnimation = 45;
+            Item.useTime = 45;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            // 生成条件
+            return true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            SoundEngine.PlaySound(SoundID.Roar, player.Center);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+                NPC.SpawnOnPlayer(player.whoAmI, NPCID.BlueSlime);
+            else
+                NetMessage.SendData(MessageID.SpawnBossUseLicenseStartEvent, -1, -1, null, player.whoAmI, NPCID.BlueSlime);
+
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ModContent.ItemType<Materials.Neuro>(), 15).
+                AddIngredient(ModContent.ItemType<VoidPowder>(), 30).
+                AddTile(TileID.DemonAltar).
+                Register();
+            CreateRecipe().
+                AddIngredient(ModContent.ItemType<Materials.Neuro>(), 15).
+                AddIngredient(ModContent.ItemType<VoidPowder>(), 30).
+                AddTile(ModContent.TileType<RevivalAltar>()).
+                Register();
+            return;
+        }
+    }
+}
---
+++ Content/Items/Materials/VoidPowder.cs
@@ -0,0 +1,52 @@
+﻿//#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBHMODE.Content.Items.Materials
+{
+    public class VoidPowder : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 99;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 69;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 22;
+            Item.height = 26;
+            Item.maxStack = 9999;
+            Item.value = Item.buyPrice(silver: 1); // 售价
+            Item.rare = ItemRarityID.White;
+            Item.useTurn = true;
+            Item.autoReuse = false;
+            Item.consumable = true;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.useStyle = ItemUseStyleID.Swing;
+        }
+        public override bool? UseItem(Player player)
+        {
+            return true;
+        }
+        public override void AddRecipes()
+        {
+#if DEBUG
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.DirtBlock, 1); // 合成配方
+            recipe.AddTile(TileID.WorkBenches); // 合成台
+            recipe.ReplaceResult(ModContent.ItemType<Neuro>(), 100);
+            recipe.Register();
+#endif
+            return;
+        }
+    }
+}
