aa97c7790078c4c5a395704f6713b04cac3bae8f
wbh-tmod <wbh123456kevin@qq.com>
2026-01-03 19:26:30 +0800

    新增了消蚀之地生态（判定条件：20个流动石块） 为流动石块FlowingStone同时配置了开发者后门 生态只会有音乐发生变化，检测条件：大于20块，地表 版本号更新至0.1.12

---
+++ Common/ErodedLandCondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using WBHMODE.Content.Biomes;
+
+namespace WBHMODE.Common
+{
+    public static class ErodedLandCondition
+    {
+        public static Condition InErodedLandBiome = new Condition("Mods.WBHMODE.Conditions.InErodedLandBiome", () => Main.LocalPlayer.InModBiome<ErodedLandBiome>() /*|| Main.LocalPlayer.InModBiome<ErodedLandUndergroundBiome>()*/);
+
+    }
+}
