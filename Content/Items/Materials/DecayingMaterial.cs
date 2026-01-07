62ea2c95d66e247a720f90bd34ae3d26b5dab2bf
lxklp <andrew20040528@163.com>
2026-01-07 19:33:43 +0800

    添加半衰质

---
+++ Content/Items/Materials/DecayingMaterial.cs
@@ -0,0 +1,38 @@
+﻿//#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using WBHMODE.Content.Tiles;
+
+namespace WBHMODE.Content.Items.Materials
+{
+    public class DecayingMaterial: ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 25;
+            ItemID.Sets.SortingPriorityMaterials[Type] = 69; // Hellstone
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.maxStack = 9999;
+            Item.value = Item.buyPrice(silver: 45); // 售价
+            Item.rare = ItemRarityID.Orange;
+            Item.useTurn = true;
+            Item.autoReuse = false;
+            Item.consumable = false;
+        }
+        public override void AddRecipes()
+        {
+            return;
+        }
+    }
+}
