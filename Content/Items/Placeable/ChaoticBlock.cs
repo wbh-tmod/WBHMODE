d188f1574dd27276fcc88e6ceaca73dc821ae0fe
lxklp <andrew20040528@163.com>
2026-01-10 22:42:21 +0800

    加入了错乱块

---
+++ Content/Items/Placeable/ChaoticBlock.cs
@@ -0,0 +1,36 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBHMODE.Content.Items.Placeable
+{
+    public class ChaoticBlock : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+            ItemID.Sets.ExtractinatorMode[Item.type] = Item.type;
+        }
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.ChaoticBlock>());
+            Item.width = 12;
+            Item.height = 12;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            // 未做：合成表，如从错乱块平台回到错乱块。
+            // 未做原因：其他物品还未实现。
+            return;
+        }
+    }
+}
--- Content/Tiles/ChaoticBlock.cs
+++ Content/Tiles/ChaoticBlock.cs
@@ -12,23 +12,14 @@ using WBHMODE.Content.Projectiles;
 
 namespace WBHMODE.Content.Tiles
 {
-    public class SculptureWood : ModTile
+    public class ChaoticBlock : ModTile
     {
         public override void SetStaticDefaults() {
             Main.tileSolid[Type] = true;
             Main.tileMergeDirt[Type] = true;
             Main.tileBlockLight[Type] = true;
-            TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Wood"]);
-
-            HitSound = SoundID.Dig;
-            AnimationFrameHeight = 270;
-            DustType = DustID.WoodFurniture;
+            DustType = DustID.SnowBlock;
             AddMapEntry(new Color(150, 150, 150));
-        }
-        public override bool CreateDust(int i, int j, ref int type)
-        {
-            Dust.NewDust(new Vector2(i, j) * 16f, 16, 16, DustID.WoodFurniture, 0f, 0f, 1, new Color(255, 255, 255), 1f);
-            return false;
         }
     }
 }
