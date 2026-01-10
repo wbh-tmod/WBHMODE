cdf71bb54215c23bb610c11a4138572dcb98f424
lxklp <andrew20040528@163.com>
2026-01-10 22:25:22 +0800

    添加了雕塑木

---
+++ Content/Items/Placeable/SculptureWood.cs
@@ -0,0 +1,37 @@
+﻿#define DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WBHMODE.Content.Items.Placeable
+{
+    public class SculptureWood : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 100;
+            ItemID.Sets.ExtractinatorMode[Item.type] = Item.type;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.Wood;
+        }
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<Tiles.SculptureWood>());
+            Item.width = 12;
+            Item.height = 12;
+        }
+
+        // Please see Content/ExampleRecipes.cs for a detailed explanation of recipe creation.
+        public override void AddRecipes()
+        {
+            // 未做：合成表，如从雕塑木平台回到雕塑木。
+            // 未做原因：其他物品还未实现。
+            return;
+        }
+    }
+}
---
+++ Content/Tiles/SculptureWood.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.Metadata;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WBHMODE.Content.Projectiles;
+
+namespace WBHMODE.Content.Tiles
+{
+    public class SculptureWood : ModTile
+    {
+        public override void SetStaticDefaults() {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            TileMaterials.SetForTileId(Type, TileMaterials._materialsByName["Wood"]);
+
+            HitSound = SoundID.Dig;
+            AddMapEntry(new Color(218, 169, 97));
+            AnimationFrameHeight = 270;
+            DustType = DustID.Stone;
+            AddMapEntry(new Color(150, 150, 150));
+        }
+        public override bool CreateDust(int i, int j, ref int type)
+        {
+            Dust.NewDust(new Vector2(i, j) * 16f, 16, 16, DustID.WoodFurniture, 0f, 0f, 1, new Color(255, 255, 255), 1f);
+            return false;
+        }
+    }
+}
